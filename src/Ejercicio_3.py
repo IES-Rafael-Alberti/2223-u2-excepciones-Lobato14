4e07c0e746cb9ec01865d363978733a41af4609e
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-24 08:45:27 +0200

    Cambios realizados Ejercicio_3.py y en test_ej3.py

--- src/Ejercicio_3.py
+++ src/Ejercicio_3.py
@@ -5,24 +5,24 @@
 def cuenta_regresiva(numero, cuenta_atras):
     try:
         numero = int(numero)
-        if numero < 0:
-            raise ValueError("Por favor, ingrese un número entero positivo válido.")
-        while numero > 0:
+        if numero <= 0:
+            raise ValueError("Por favor, ingrese un número entero no negativo y mayor que 0.")
+        while numero >= 0:
             cuenta_atras.append(str(numero))
             numero -= 1
         return ", ".join(cuenta_atras)
-    except ValueError:
-        raise ValueError("Por favor, ingrese un número entero válido.")
+    except ValueError as e:
+        raise ValueError(str(e))
 
 if __name__ == "__main__":
+    # Entrada
     cuenta_atras = []
-    numero = -1
-    while numero < 0:
+    numero = None
+    # Procesamiento
+    while numero is None or numero < 0:
         try:
-            numero = int(input("Por favor, ingrese un número entero positivo: "))
-            if numero < 0:
-                print("Error: El número debe ser positivo.")
-        except ValueError:
-            print("Error: Por favor, ingrese un número entero válido.")
-    resultado = cuenta_regresiva(numero, cuenta_atras)
-    print("Cuenta regresiva desde", numero, "hasta 0:", resultado)
+            numero = int(input("Por favor, ingrese un número entero no negativo: "))
+            resultado = cuenta_regresiva(numero, cuenta_atras)
+            print("Cuenta regresiva desde", numero, "hasta 0:", resultado)
+        except ValueError as e:
+            print("Error:", e)
